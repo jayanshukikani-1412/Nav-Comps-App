81d3752232eb6424624c0d5353a7b1697f928971
Jayanshu <jayanshu.crawlapps@gmail.com>
2023-07-03 14:28:22 +0530

    Some Project Organixation is created.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import React from 'react'
-import Button from './Button';
-import { GoBellFill, GoCodeOfConduct,GoCheckCircleFill } from "react-icons/go";
-const App = () => {
-    const handleClick = () => {
-        console.log("BUTTON IS CLICKED...")
-    }
-    const handleMouseOver = () => {
-        console.log("BUTTON IS HOVER...")
-    }
+import ButtonPage from './Page/ButtonPage';
 
+const App = () => {
     return (
         <div>
-            <div>
-                <Button success rounded outline onClick={handleClick} className="mb-10">
-                    <GoBellFill />
-                    Click Here
-                </Button>
-            </div>
-            <div>
-                <Button warning onMouseOver={handleMouseOver}>
-                    <GoCodeOfConduct />
-                    Hello
-                </Button>
-            </div>
-            <div>
-                <Button secondary outline>
-                    Good Morning
-                </Button>
-            </div>
-            <div>
-                <Button primary rounded>
-                    How are you ?
-                </Button>
-            </div>
-            <div>
-                <Button danger>
-                <GoCheckCircleFill />
-                    I am fine.
-                </Button>
-            </div>
+            <ButtonPage/>
         </div>
     )
 }
---
+++ src/Page/ButtonPage.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { GoBellFill, GoCodeOfConduct,GoCheckCircleFill } from "react-icons/go";
+import Button from '../Component/Button';
+
+const ButtonPage = () => {
+    const handleClick = () => {
+        console.log("BUTTON IS CLICKED...")
+    }
+    const handleMouseOver = () => {
+        console.log("BUTTON IS HOVER...")
+    }
+
+    return (
+        <div>
+            <div>
+                <Button success rounded outline onClick={handleClick} className="mb-10">
+                    <GoBellFill />
+                    Click Here
+                </Button>
+            </div>
+            <div>
+                <Button warning onMouseOver={handleMouseOver}>
+                    <GoCodeOfConduct />
+                    Hello
+                </Button>
+            </div>
+            <div>
+                <Button secondary outline>
+                    Good Morning
+                </Button>
+            </div>
+            <div>
+                <Button primary rounded>
+                    How are you ?
+                </Button>
+            </div>
+            <div>
+                <Button danger>
+                <GoCheckCircleFill />
+                    I am fine.
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default ButtonPage;
+
