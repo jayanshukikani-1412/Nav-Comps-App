5512578f82870b69e0d604c5e2425440f1522c7f
Jayanshu <jayanshu.crawlapps@gmail.com>
2023-07-03 11:37:34 +0530

    Tailwind css add and classnames js library is installed and used.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import Button from './Button';
 const App = () => {
-  return (
-    <div>
-      <div>
-        <Button success rounded outline>Click Here</Button>
-      </div>
-      <div>
-        <Button warning>Hello</Button>
-      </div>
-      <div>
-        <Button secondary outline>Good Morning</Button>
-      </div>
-      <div>
-        <Button primary rounded >How are you ?</Button>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <Button success rounded outline>Click Here</Button>
+            </div>
+            <div>
+                <Button warning>Hello</Button>
+            </div>
+            <div>
+                <Button secondary outline>Good Morning</Button>
+            </div>
+            <div>
+                <Button primary rounded >How are you ?</Button>
+            </div>
+            <div>
+                <Button danger >I am fine.</Button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types' 
+import className from 'classnames'
 
 const Button = ({
     children,
@@ -11,9 +12,23 @@ const Button = ({
     outline,
     rounded
 }) => {
+    const classes = className('px-6 py-1.5 border m-2', {
+        'border-blue-600 bg-blue-500 text-white':primary,
+        'border-gray-600 bg-gray-500 text-white':secondary,
+        'border-green-600 bg-green-500 text-white':success,
+        'border-yellow-600 bg-yellow-500 text-white':warning,
+        'border-red-600 bg-red-500 text-white':danger,
+        'rounded-full':rounded,
+        'bg-white':outline,
+        'text-blue-500':outline && primary,
+        'text-gray-500':outline && secondary,
+        'text-green-500':outline && success,
+        'text-yellow-500':outline && warning,
+        'text-red-500':outline && danger,
+    })
 
     return (
-        <button>{children}</button>
+        <button className={classes}>{children}</button>
     )
 }
 
@@ -26,12 +41,12 @@ Button.propTypes = {
         danger
     }) => {
         const count = Number(!!primary)
-        + Number(!!secondary)
-        + Number(!!success)
-        + Number(!!warning)
-        + Number(!!danger)
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
 
-        if (count > 1){
+        if (count > 1) {
             return new Error('!!! ONLY ONE OD PRIMARY, SECONDARY, SUCCESS, WARNING, DANGER CAN BE TRUE !!!')
         }
     }
