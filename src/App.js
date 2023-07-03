24c962ba8998030739f6cf28664975c73126d617
Jayanshu <jayanshu.crawlapps@gmail.com>
2023-07-03 10:40:34 +0530

    PropTypes Library Is added and Tailwind Css setup process is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Button from './Button';
 const App = () => {
   return (
     <div>
-      App
+      <div>
+        <Button success rounded outline>Click Here</Button>
+      </div>
+      <div>
+        <Button warning>Hello</Button>
+      </div>
+      <div>
+        <Button secondary outline>Good Morning</Button>
+      </div>
+      <div>
+        <Button primary rounded >How are you ?</Button>
+      </div>
     </div>
   )
 }
